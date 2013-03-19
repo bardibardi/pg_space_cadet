b5ab9461340511cd6dac00a607958455324ea6b4
Bardi Einarsson <bardi_e@hotmail.com>
2013-03-19 16:50:24 -0700

    SpaceCadet::Uuid.new.id uniqueness assured version 0.7.0

--- lib/space_cadet_active_record_uuid.rb
+++ lib/space_cadet_active_record_uuid.rb
@@ -7,7 +7,7 @@ require 'active_record'
 
 module SpaceCadetActiveRecordUuid
 
-  def id_add_uuid uuid_class, source_name, id, uuid
+  def add_uuid uuid_class, source_name, id, uuid
     conn = uuid_class.connection
     table_name = uuid_class.table_name
     set_auto_increment conn, table_name, id
@@ -17,13 +17,26 @@ module SpaceCadetActiveRecordUuid
     end
   end
 
-  def prepare_create uuid_class, record, id_bit_count
-    source_name = record.class.table_name
-    conn = record.class.connection
+  def id_add_uuid uuid_class, source_name, id_bit_count
+    conn = uuid_class.connection
     uuid = new_uuid conn
     id = id_from_uuid uuid, id_bit_count
-    id_add_uuid uuid_class, source_name, id, uuid
-    set_auto_increment conn, source_name, id
+    record = nil
+    begin
+      record = add_uuid uuid_class, source_name, id, uuid
+    rescue
+      record = nil
+    end
+    record
+  end
+
+  def prepare_create uuid_class, record, id_bit_count
+    source_name = record.class.table_name
+    record = id_add_uuid uuid_class, source_name, id_bit_count
+    record = id_add_uuid uuid_class, source_name, id_bit_count unless record
+    record = id_add_uuid uuid_class, source_name, id_bit_count unless record
+    conn = record.class.connection
+    set_auto_increment conn, source_name, record.id
   end
 
 end # SpaceCadetActiveRecordUuid
--- spec/space_cadet_spec.rb
+++ spec/space_cadet_spec.rb
@@ -1,11 +1,28 @@
 require_relative 'support/ar_rspec'
 require_relative 'support/space_cadet'
+require 'securerandom'
 
 describe SpaceCadet::Uuid do
+
   it "should be created" do
-    id, record = add_chess_game 'e2e4', ''
+    id, _ = add_chess_game 'e2e4', ''
     u = SpaceCadet::Uuid.find(id)
     expect(id).to eq(u.id)
   end
+
+  it "should be unique" do
+    duplicate_id, chess_game = add_chess_game 'e2e4', ''
+    u = SpaceCadet::Uuid.find(duplicate_id)
+    duplicate_uuid = u.uuid
+    exception_thrown = nil
+    begin
+      chess_game.add_uuid(SpaceCadet::Uuid.class,
+        'chess_games', duplicate_id, duplicate_uuid)
+    rescue
+      exception_thrown = true
+    end
+    expect(exception_thrown).to eq(true)
+  end
+
 end
 
