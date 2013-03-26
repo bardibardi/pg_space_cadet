ad3a323c3a68a29ed4b78e5cb7b5594591134daa
Bardi Einarsson <bardi_e@hotmail.com>
2013-03-26 11:49:52 -0700

    uniqueness test passing for wrong reason

--- spec/space_cadet_spec.rb
+++ spec/space_cadet_spec.rb
@@ -14,9 +14,16 @@ describe SpaceCadet::Uuid do
     duplicate_id, chess_game = add_chess_game 'e2e4', ''
     u = SpaceCadet::Uuid.find(duplicate_id)
     duplicate_uuid = u.uuid
+    scw = SpaceCadetWrapper.new
+    fake_uuid = SecureRandom.uuid
+    fake_id = scw.id_from_uuid fake_uuid, 31
+    # to make sure that the expected exception
+    #   is not due to broken code
+    scw.add_uuid(SpaceCadet::Uuid,
+      'chess_games', fake_id, fake_uuid)
     exception_thrown = nil
     begin
-      chess_game.add_uuid(SpaceCadet::Uuid.class,
+      scw.add_uuid(SpaceCadet::Uuid,
         'chess_games', duplicate_id, duplicate_uuid)
     rescue
       exception_thrown = true
